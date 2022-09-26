73fbf2995bc4d9d73b20a4d0de02e3d4f9a6a47c
CtrlShiftSammy <kaustav_b@ph.iitr.ac.in>
2022-09-26 16:08:23 +0530

    Revert "Revert "Merge pull request #1 from SuryanshBisht/main""
    This reverts commit 73774125b52f99da6433984c5f4ed139a89df3e1.

---
+++ src/assets/data/instruments_data.js
@@ -0,0 +1,75 @@
+const instruments_data = [
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Celestron Omni XLT 120 6\" ",
+        desc : "A 6 inch refractor with a focal length of 1000mm, ",
+        key : 1,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Meade LX90 ACF 8\"",
+        desc : "An 8\" reflector with completely automated equitorial movement, comes witha 8x50 Scope with a focal legth of 2000mm",
+        key : 2,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Nikon Action 7x50 ",
+        desc : "A rugged pair of 7x50mm binoculars for everyday viewing.",
+        key : 3,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "1.25\" Eyepices",
+        desc : "32mm, 17mm, 13mm, 6mm and 2x barlows",
+        key : 4,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Light Filters",
+        desc : "Red, Yellow, Light and Dark Green, Blue and Orange Color FIlters with a Light pollution and Moon Filter for 1.25\" eyepieces",
+        key : 5,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Nikon D5600",
+        desc : "A digital single-lens reflex camera",
+        key : 6,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Arduino G521",
+        desc : "Gyroscope",
+        key : 7,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Node MCU-ESP8266",
+        desc : "An Internet of Things platform.",
+        key : 8,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Node MCU-ESPWROOM",
+        desc : "A WiFi based System-on-Chip with bluetooth",
+        key : 9,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "RTL-SDR",
+        desc : "A versatile SDR, with many RF Applications",
+        key : 10,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Bias Tee",
+        desc : "Mini Circuits ZFBT-4R2G-FT Bias Tee for RF applications",
+        key : 11,
+    },
+    {
+        src : "https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        title : "Humidity and Altitude sensors",
+        desc : " ",
+        key : 12,
+    },
+];
+export default instruments_data;
